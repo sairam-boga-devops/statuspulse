ab3de620f9d9a71b7860a1ff945a65e04aa495eb
Sai <saiboga848@gmail.com>
2026-05-14 08:21:33 +0530

    health-monitor.sh file added

---
+++ scripts/health-monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+APP_URL="https://statuspulse-aws.duckdns.org/health"
+LOG_FILE="$HOME/health-monitor.log"
+
+TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+
+STATUS=$(curl -s -o /dev/null -w "%{http_code}" $APP_URL)
+
+if [ "$STATUS" -eq 200 ]; then
+    echo "$TIMESTAMP - Application is healthy." >> $LOG_FILE
+else
+    echo "$TIMESTAMP - Application health check FAILED." >> $LOG_FILE
+fi
